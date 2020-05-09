16b0c4ca432f8fa072f85bda221f367b6140a976
Ally-MF <angelaflaquer@gmail.com>
2020-05-09 11:40:13 -0400

    Add new files

--- example.rb
+++ example.rb
@@ -1,8 +1,12 @@
 def turn_of
+    do = true
+    lights = 'on after midnight'
     return false
 end
 
 def turn_on
     dont_do = false
+    performance = 'angela off after noon'
+    candy = 'this is good for you'
     return true
 end
